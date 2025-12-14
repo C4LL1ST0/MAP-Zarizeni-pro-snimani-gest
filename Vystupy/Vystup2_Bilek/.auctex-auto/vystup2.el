840a365cec39485fd2637b77a380e299dc7986db
Štěpán Bílek <stepan@localhost.localdomain>
2025-12-14 15:57:28 +0100

    fix: vystup 2 optim

--- Vystupy/Vystup2_Bilek/.auctex-auto/vystup2.el
+++ Vystupy/Vystup2_Bilek/.auctex-auto/vystup2.el
@@ -7,12 +7,12 @@
                      '(("article" "11pt" "a4paper")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("inputenc" "utf8") ("babel" "czech") ("url" "hyphens") ("hyperref" "colorlinks=true" "urlcolor=blue" "linkcolor=black" "breaklinks" "citecolor=black") ("graphicx" "") ("float" "") ("biblatex" "backend=biber")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
